f6bb83cb4d66d8d8e73aef143f9e2815f768f2fc
altwake <145942324+altwake@users.noreply.github.com>
2024-05-27 05:16:33 -0400

    Co-authored-by: altanan04@gmail.com <altanan04@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var today = new Date();
+
+console.log*today;
+
+var thisYear = getFullYear;
+
+var footer = document.querySelector('footer');
+
+var copyright = document.createElement('p');
+
+copyright.innerHTML = `&copy; Your Name ${thisYear}`;
+
+footer.appendChild(copyright);
+
+var skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
+
+var skillsSection = document.getElementById('skills-section');
+
+var skillsList = skillsSection.querySelector('ul');
+
+for (var i = 0; i < skills.length; i++) {
+    var skill = document.createElement('li');
+    
+    skill.innerText = skills[i];
+    
+    skillsList.appendChild(skill);
+}
