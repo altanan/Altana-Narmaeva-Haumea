d458a3f6b9aa126fd3ac37446d45ff9f29c86368
altwake <145942324+altwake@users.noreply.github.com>
2024-05-29 01:16:46 -0400

    extra changes

--- js/index.js
+++ js/index.js
@@ -31,6 +31,7 @@ messageForm.addEventListener('submit', function(event) {
     var usersEmail = event.target.usersEmail.value;
     var usersMessage = event.target.usersMessage.value;
 
+    //fill in form to be able to message
     console.log("Name:", usersName);
     console.log("Email:", usersEmail);
     console.log("Message:", usersMessage);
@@ -40,23 +41,55 @@ messageForm.addEventListener('submit', function(event) {
     //be able to message the messenger
     var newMessage = document.createElement('li');
 
-    newMessage.innerHTML = '<a href="mailto:' + usersEmail + '">' +  usersName + '</a>' +
-                           '<span>' + usersMessage + '</span>';
-
+    newMessage.innerHTML = '<a href="mailto:' + usersEmail + '">' + usersName + '</a>' +
+                           '<span>: ' + usersMessage + '</span>'; 
+    //remove message
     var removeButton = document.createElement('button');
-
+    
     removeButton.innerText = 'remove';
     removeButton.type = 'button';
-
+    
     removeButton.addEventListener('click', function() {
         var entry = removeButton.parentNode;
         entry.remove();
     });
-
+    
     newMessage.appendChild(removeButton);
-
+    
     messageList.appendChild(newMessage);
 });
 
 var messageSection = document.querySelector('#messages');
 var messageList = messageSection.querySelector('ul');
+
+//takes github profile
+const GITHUB_USERNAME = 'altanan';
+var projectSection = document.getElementById('projects');
+var projectList = projectSection.querySelector('ul');
+ 
+//displays repos from github profile
+fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const repositories = data.map(repo => ({
+            name: repo.name,
+            description: repo.description,
+            url: repo.html_url
+        }));
+        console.log(repositories);
+
+        repositories.forEach(repo => {
+            var projectItem = document.createElement('li');
+            projectItem.innerHTML = `<a href="${repo.url}" target="_blank">${repo.name}</a>`;
+            projectList.appendChild(projectItem);
+        });
+    })
+    .catch(error => {
+        //error message just in case
+        console.error('There was a problem with the fetch operation:', error);
+    });
